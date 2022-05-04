2b03ae39f3eaf979ceb5bfc494c34a94d2b1007f
alanmac17 <alan17wilson@gmail.com>
2022-05-04 06:21:03 +0000

    create variables for elements in html document

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,28 @@
-const quizPanel = document.getElementById('')
+// todays date
+const issueDate = document.getElementById('todays-date')
+
+// questions & answers
+const questionNo = document.getElementById('current-question-no')
+const currentQuestion = document.getElementById('question-area')
+const currentAnswerOptions = document.getElementById('answers-area')
+
+// buttons
+const button0 = getElementById('btn-answ0')
+const button2 = getElementById('btn-answ1')
+const button3 = getElementById('btn-answ2')
+const button4 = getElementById('btn-answ3')
+
+// answers
+const answer0 = getElementById('answer0')
+const answer1 = getElementById('answer1')
+const answer2 = getElementById('answer2')
+const answer3 = getElementById('answer3')
+
+// navigation
+const previousq = document.getElementById('previous')
+const nextq = document.getElementById('next')
+const submitq = document.getElementById('submit')
+
+// Initial message and scoreboard
+var messageBoard = document.getElementById('story-text')
+
