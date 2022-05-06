045466881af04c7d8e2efbdf10a0d7f2a47f61be
alanmac17 <alan17wilson@gmail.com>
2022-05-06 12:21:03 +0000

    debug create quiz function

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,15 +2,14 @@
 const issueDate = document.getElementById('todays-date')
 
 // questions & answers
-var questionNo = document.getElementById('current-question-no')
-var currentQuestion = document.getElementById('question-area')
-var currentAnswerOptions = document.getElementById('answers-area')
-
+const questionindex = document.getElementById('current-question-no')
+const currentQuestion = document.getElementById('questionArea')
+const currentAnswerOptions = document.getElementById('answers-area')
 
 // answers
-const answer0 = document.getElementById('answer0')
-const answer1 = document.getElementById('answer1')
-const answer2 = document.getElementById('answer2')
+const answer0 = document.getElementById('answer0') 
+const answer1 = document.getElementById('answer1') 
+const answer2 = document.getElementById('answer2') 
 const answer3 = document.getElementById('answer3')
 
 // navigation
@@ -21,12 +20,15 @@ const submitQuiz = document.getElementById('submit')
 // Initial message and scoreboard
 var messageBoard = document.getElementById('story-text')
 
-// scoring
-var yourScore = document.getElementById("user-score");
-var totalScore = document.getElementById("total-score");
+// Variables
+
+let currentIndex = 0;
+let answeredQuestions =[]; // array of anwered question indexes
+let score = 0;
 
 
-let questionsList = [
+// Questions
+let questions = [
     {
         question: "What is the Capital of Nigeria?",
         a: "Cape Town",
@@ -62,34 +64,35 @@ let questionsList = [
 
 ]
 
+
+
+
 // functions
 function createQuiz() {
 
-currentQuestion = 0;
-// totalScore.innerHTML = questionsList.length;
-
-const currentQuizFile = questionsList[currentQuestion]
-
-currentQuestion.innerText = currentQuizFile.question
-answer0.innerText = currentQuizFile.a
-answer1.innerText = currentQuizFile.b
-answer2.innerText = currentQuizFile.c
-answer3.innerText = currentQuizFile.d
-
+questionindex.innerHTML = currentIndex + 1
+currentQuestion.innerText = questions[currentIndex].question;
+answer0.innerText = questions[currentIndex].a
+answer1.innerText = questions[currentIndex].b
+answer2.innerText = questions[currentIndex].c
+answer3.innerText = questions[currentIndex].d
+currentIndex++
 }
 
-createQuiz();
+createQuiz()
+
 
 function showFinalScore() {
 
 }
 
 
-function checkAnswer() {}
+function checkAnswer() {
+
+}
 
 
 // Event Listeners
 submitQuiz.addEventListener("click", showFinalScore);
 previousq.addEventListener("click", );
 nextq.addEventListener("click", nextQuestion);
-submitQuiz.addEventListener("click",);
