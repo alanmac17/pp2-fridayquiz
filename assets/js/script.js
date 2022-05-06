aa64a10209b86030b21a0a851c3e0fc01874b87a
alanmac17 <alan17wilson@gmail.com>
2022-05-06 22:00:07 +0000

    hide previous question button on start

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,9 +7,9 @@ const currentQuestion = document.getElementById('questionArea')
 const currentAnswerOptions = document.getElementById('answers-area')
 
 // answers
-const answer0El = document.getElementById('answerA') 
-const answer1El = document.getElementById('answerB') 
-const answer2El = document.getElementById('answerC') 
+const answer0El = document.getElementById('answerA')
+const answer1El = document.getElementById('answerB')
+const answer2El = document.getElementById('answerC')
 const answer3El = document.getElementById('answerD')
 
 // navigation
@@ -27,15 +27,14 @@ let Quizscore = 0;
 
 
 // Questions
-let questions = [
-    {
+let questions = [{
         question: "What is the Capital of Nigeria?",
         a: "Cape Town",
         b: "Lagos",
         c: "Kano",
         d: "Zaria",
         correct: "answerB",
-        usersAnswer:null,
+        usersAnswer: null,
     },
     {
         question: "Who collaborated with Elton John on Cold Hearts",
@@ -44,7 +43,7 @@ let questions = [
         c: "Robyn",
         d: "Dua Lipa",
         correct: "answerD",
-        usersAnswer:null,
+        usersAnswer: null,
     },
     {
         question: "Which actor played the Joker in the 2022 film, The Batman?",
@@ -53,7 +52,7 @@ let questions = [
         c: "Peter MacDonald",
         d: "Andy Serkins",
         correct: "answerA",
-        usersAnswer:null,
+        usersAnswer: null,
     },
     {
         question: "The Premier of which islands was accused of cocaine trafficing?",
@@ -62,49 +61,62 @@ let questions = [
         c: "The Cook Islands",
         d: "The British Virgin Islands",
         correct: "answerD",
-        usersAnswer:null,
+        usersAnswer: null,
     },
 
 ]
 
 
 // functions
+
+//Function to create a question by passing question index and pulling rel data from question array
 function createQuestion(questionIndex) {
 
-questionIndexEl.innerHTML = questionIndex + 1
-currentQuestion.innerText = questions[questionIndex].question;
+    questionIndexEl.innerHTML = questionIndex + 1
+    currentQuestion.innerText = questions[questionIndex].question;
 
-answer0El.innerText = questions[questionIndex].a
-answer1El.innerText = questions[questionIndex].b
-answer2El.innerText = questions[questionIndex].c
-answer3El.innerText = questions[questionIndex].d
+    answer0El.innerText = questions[questionIndex].a
+    answer1El.innerText = questions[questionIndex].b
+    answer2El.innerText = questions[questionIndex].c
+    answer3El.innerText = questions[questionIndex].d
 
 }
 
 createQuestion(currentIndex)
 
-function nextQuestion()
-{
-currentIndex++
-createQuestion(currentIndex)
+//Function to populate next question with incremental currentIndex value that is then passed to CreateQuestion
 
-if (currentIndex === questions.length-1) {
-    submitQuizEl.classList.remove("hide")
-}
-}
+function nextQuestion() {
+    currentIndex++
+    createQuestion(currentIndex)
 
-function previousQuestion()
-{
-currentIndex--
-createQuestion(currentIndex)
-submitQuizEl.classList.add("hide")
-
+    if (currentIndex === questions.length - 1) {
+        submitQuizEl.classList.remove("hide")
+        nextQuestionEl.classList.add("hide")
+    } else if (currentIndex > 0) {
+        previousQuestionEl.classList.remove("hide")
+    }
 }
 
+//Function to populate previous question with decreased currentIndex value that is then passed to CreateQuestion
+
+function previousQuestion() {
+    currentIndex--
+    createQuestion(currentIndex)
+    submitQuizEl.classList.add("hide")
+    nextQuestionEl.classList.remove("hide")
+}
 
 function showFinalScore() {
 
+
+   
+    //if the user answer was correct then highlight the answer id button
+    //else if the user answer was incorrect highlight the answer id button and highlight the user answer id button
+
 }
+
+//Function to add the users selected or clicked answer to the questions array by using the target id from the event that was passed through, 
 
 function addAnswer(event) {
     const buttonEl = event.target
@@ -112,11 +124,14 @@ function addAnswer(event) {
     console.log(questions[currentIndex])
 }
 
+
+
 // Event Listeners
 submitQuizEl.addEventListener("click", showFinalScore);
-previousQuestionEl.addEventListener("click",previousQuestion);
+previousQuestionEl.addEventListener("click", previousQuestion);
 nextQuestionEl.addEventListener("click", nextQuestion);
 
+// Event Listeners to capture the click action and run addAnswer
 answer0El.addEventListener("click", addAnswer);
 answer1El.addEventListener("click", addAnswer);
 answer2El.addEventListener("click", addAnswer);
